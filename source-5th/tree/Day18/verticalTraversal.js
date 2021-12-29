d529ef993694cbf165057f2618d3843b8d1f0c7b
MissNanLan <nanlan.yj@foxmail.com>
2021-12-29 22:57:10 +0800

    feat: 987. 二叉树的垂序遍历

--- source-5th/tree/Day18/verticalTraversal.js
+++ source-5th/tree/Day18/verticalTraversal.js
@@ -6,57 +6,42 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-/**
- * @param {TreeNode} root
- * @return {number[][]}
- */
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[][]}
- */
  var verticalTraversal = function (root) {
-    if (!root) return [];
-  
-    // 坐标集合以 x 坐标分组
-    const pos = {};
-    // dfs 遍历节点并记录每个节点的坐标
-    dfs(root, 0, 0);
-  
-    // 得到所有节点坐标后，先按 x 坐标升序排序
-    let sorted = Object.keys(pos)
-      .sort((a, b) => +a - +b)
-      .map((key) => pos[key]);
-  
-    // 再给 x 坐标相同的每组节点坐标分别排序
-    sorted = sorted.map((g) => {
-      g.sort((a, b) => {
-        // y 坐标相同的，按节点值升序排
-        if (a[0] === b[0]) return a[1] - b[1];
-        // 否则，按 y 坐标降序排
-        else return b[0] - a[0];
-      });
-      // 把 y 坐标去掉，返回节点值
-      return g.map((el) => el[1]);
+  if (!root) return [];
+
+  // 坐标集合以 x 坐标分组
+  const pos = {};
+  // dfs 遍历节点并记录每个节点的坐标
+  dfs(root, 0, 0);
+
+  // 得到所有节点坐标后，先按 x 坐标升序排序
+  let sorted = Object.keys(pos)
+    .sort((a, b) => +a - +b)
+    .map((key) => pos[key]);
+
+  // 再给 x 坐标相同的每组节点坐标分别排序
+  sorted = sorted.map((g) => {
+    g.sort((a, b) => {
+      // y 坐标相同的，按节点值升序排
+      if (a[0] === b[0]) return a[1] - b[1];
+      // 否则，按 y 坐标降序排
+      else return b[0] - a[0];
     });
-  
-    return sorted;
-  
-    // *********************************
-    function dfs(root, x, y) {
-      if (!root) return;
-  
-      x in pos || (pos[x] = []);
-      // 保存坐标数据，格式是: [y, val]
-      pos[x].push([y, root.val]);
-  
-      dfs(root.left, x - 1, y - 1);
-      dfs(root.right, x + 1, y - 1);
-    }
-  };
+    // 把 y 坐标去掉，返回节点值
+    return g.map((el) => el[1]);
+  });
+
+  return sorted;
+
+  // *********************************
+  function dfs(root, x, y) {
+    if (!root) return;
+
+    x in pos || (pos[x] = []);
+    // 保存坐标数据，格式是: [y, val]
+    pos[x].push([y, root.val]);
+
+    dfs(root.left, x - 1, y - 1);
+    dfs(root.right, x + 1, y - 1);
+  }
+};
