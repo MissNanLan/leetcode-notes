365b1cefe840868706c337522237798599ee4027
MissNanLan <nanlan.yj@foxmail.com>
2022-01-17 23:09:57 +0800

    69. Sqrt(x))

---
+++ source-6th/TwoPoints/Day37/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+var mySqrt = function (x) {
+  let l = 0,
+    r = x,
+    ans = -1;
+  while (l <= r) {
+      const mid = Math.floor((l + r) / 2)
+    //   const mid = ((l+r)>>2)
+    if (mid * mid <= x) {
+      ans = mid;
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+  return ans;
+};
+
+console.log(mySqrt(8));
