f48cac2e4737ec0c060783c99515fa9911d2e734
MissNanLan <nanlan.yj@foxmail.com>
2022-01-20 23:02:19 +0800

    feat: Minimum Light Radius

--- source-6th/TwoPoints/Day40/index.js
+++ source-6th/TwoPoints/Day40/index.js
@@ -1,29 +1,58 @@
+// function solve(nums) {
+//     console.log(nums);
+//     if (nums.length < 3) return 0
+//     nums.sort((a, b) => a - b)
+//     let l = 0, r = nums[nums.length - 1]
+//     while (l <= r) {
+//         const m = l + ((r - l) >>> 1)
+//         if (find(m, nums)) r = m - 1
+//         else l = m + 1
+//     }
+//     if (l > nums[nums.length - 1] || !find(l, nums)) return -1
+//     return l / 2
+// }
 
-function solve(nums) {
-    console.log(nums);
-    if (nums.length < 3) return 0
-    nums.sort((a, b) => a - b)
-    let l = 0, r = nums[nums.length - 1]
+// function find(mid, nums) {
+//     let range = nums[0] + mid, ligths = 0
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] > range) {
+//             ligths += 1
+//             range = nums[i] + mid
+//         }
+//     }
+
+//     return ligths <= 2
+// }
+
+class Solution {
+  solve(nums) {
+    if (nums.length < 3) return 0;
+    nums.sort((a, b) => a - b);
+    let l = 0,
+      r = nums[nums.length - 1];
     while (l <= r) {
-        const m = l + ((r - l) >>> 1)
-        if (find(m, nums)) r = m - 1
-        else l = m + 1
+      const m = l + ((r - l) >>> 1);
+      if (this.find(m, nums)) r = m - 1;
+      else l = m + 1;
     }
-    if (l > nums[nums.length - 1] || !find(l, nums)) return -1
-    return l / 2
-}
+    if (l > nums[nums.length - 1] || !this.find(l, nums)) return -1;
+    return l / 2;
+  }
 
-function find(mid, nums) {
-    let range = nums[0] + mid, ligths = 0
-
+  find(mid, nums) {
+    let range = nums[0] + mid,
+      ligths = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > range) {
-            ligths += 1
-            range = nums[i] + mid
-        }
+      if (nums[i] > range) {
+        ligths += 1;
+        range = nums[i] + mid;
+      }
     }
 
-    return ligths <= 2
+    return ligths <= 2;
+  }
 }
-
-console.log(solve([1,2,95,96,97,100]))
+const res = new Solution().solve([1, 2, 95, 96, 97, 100]);
+console.log(res);
+// console.log(solve([1,2,95,96,97,100]))
